a75bc83f7d86f07582b3915bdef1716ab48381c7
zaidabukhalaf <zaidabokhalaf@gmail.com>
2020-10-20 15:25:50 +0300

    Updates

---
+++ precache-manifest.69047e25b66d59f992cd6c6b7aa210f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac4f1ca058ca11b3d7e07ff0de3cabd",
+    "url": "/covid-19-react-query-dashboard/index.html"
+  },
+  {
+    "revision": "bff16216bc9606eba8f7",
+    "url": "/covid-19-react-query-dashboard/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "fc0e30c71bbdcacd9a24",
+    "url": "/covid-19-react-query-dashboard/static/css/main.a477943d.chunk.css"
+  },
+  {
+    "revision": "bff16216bc9606eba8f7",
+    "url": "/covid-19-react-query-dashboard/static/js/2.6a458073.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-react-query-dashboard/static/js/2.6a458073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0e30c71bbdcacd9a24",
+    "url": "/covid-19-react-query-dashboard/static/js/main.99b9e90e.chunk.js"
+  },
+  {
+    "revision": "71c951ef320f8b52a221",
+    "url": "/covid-19-react-query-dashboard/static/js/runtime-main.fa8585b0.js"
+  }
+]);
